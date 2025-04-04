27a8773b5a64373cb0897a39b17f25cdddd3726b
Julian <juliantayyc@gmail.com>
2025-04-04 21:29:24 +0800

    Auto-formatted code

--- src/core/parser.py
+++ src/core/parser.py
@@ -25,7 +25,7 @@ class Parser:
         parts = message.split(self.delimiter)
         data = {}
         for part in parts:
-            if '=' not in part:
+            if "=" not in part:
                 raise ValueError(f"Malformed part in message: '{part}'")
             key, value = part.split("=")
             data[key] = value
